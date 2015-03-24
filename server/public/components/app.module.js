25f1da720de49bbda4a55ecde67e8c836bdf115e
emperorcow <emperorcow@gmail.com>
2015-03-24 10:21:31 -0500

    Added default transform response to parse date strings to objects

--- server/public/components/app.module.js
+++ server/public/components/app.module.js
@@ -15,6 +15,36 @@ cracklord.config(['growlProvider', function ($growlProvider) {
 //  growlProvider.globalDisableCountDown(true);
 }]);
 
+
+var regexISO8601 = /^(\d{4}|\+\d{6})(?:-(\d{2})(?:-(\d{2})(?:T(\d{2}):(\d{2}):(\d{2})\.(\d{1,})(Z|([\-+])(\d{2}):(\d{2}))?)?)?)?$/;
+function convertDateStringsToDates(input) {
+   if (typeof input !== "object") return input;
+
+   for (var key in input) {
+      if (!input.hasOwnProperty(key)) continue;
+
+      var value = input[key];
+      var match;
+      if (typeof value === "string" && (match = value.match(regexISO8601))) {
+         var milliseconds = Date.parse(match[0]);
+         if (!isNaN(milliseconds)) {
+            input[key] = new Date(milliseconds);
+         }
+      } else if (typeof value === "object") {
+         convertDateStringsToDates(value);
+      }
+   }
+}
+cracklord.config(["$httpProvider", function($httpProvider) {
+   $httpProvider.defaults.transformResponse.push(function(resData) {
+      convertDateStringsToDates(resData);
+      return resData;
+   });
+}]);
+
+
+
+
 cracklord.constant('USER_ROLES', {
 	admin: 'Administrator',
 	standard: 'Standard User',
