78d806470f8034986c4575684cc2e1baf415592b
emperorcow <emperorcow@gmail.com>
2015-06-18 13:25:51 -0500

    Minor changes to deb scripts

--- build/resourced/afterinstall.sh
+++ build/resourced/afterinstall.sh
@@ -5,5 +5,6 @@ mkdir -p /var/cracklord
 mkdir -p /var/log/cracklord
 chown -R cracklord:cracklord /etc/cracklord
 chown -R cracklord:cracklord /var/cracklord
+chmod -R 640 /var/cracklord
 chown -R cracklord:cracklord /var/log/cracklord
 
