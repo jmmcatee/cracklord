jmmcatee/cracklord

commit b1cf6b7bbfef60973ef72c4889830afc1ba9bca8

Resolves #44

web/components/Jobs/jobsController.js
@@ -1,6 +1,8 @@
 cracklord.controller('JobsController', ['$scope', 'JobsService', 'QueueService', 'growl', 'ResourceList', '$interval', function JobsController($scope, JobsService, QueueService, growl, ResourceList, $interval) {
 	var timer
 	$scope.listreordered = false;
+	$scope.currentjobs = [];
+	$scope.completedjobs = [];
 	ResourceList.load();
 
 	$scope.sortableOptions = {
@@ -12,19 +14,35 @@ cracklord.controller('JobsController', ['$scope', 'JobsService', 'QueueService',
 	};
 
 	$scope.loadJobs = function() {
-		$scope.jobs = JobsService.query(
+		JobsService.query(
 			//Our success handler
 			function(data) {
 				$scope.listreordered = false;
-				for(var i = 0; i < $scope.jobs.length; i++) {
-					if($scope.jobs[i].resourceid) {
-						var id = $scope.jobs[i].resourceid;
+
+				for(var i = 0; i < data.length; i++) {
+					if(data[i].resourceid) {
+						var id = data[i].resourceid;
 						var resource = ResourceList.get(id);
 						if(resource) {
-							$scope.jobs[i].resourcecolor = "background-color: rgb("+resource.color.r+","+resource.color.g+","+resource.color.b+");";
+							data[i].resourcecolor = "background-color: rgb("+resource.color.r+","+resource.color.g+","+resource.color.b+");";
 						}
 					}
-					$scope.jobs[i].expanded = false;
+					data[i].expanded = false;
+					if(items[i].status == "quit" || items[i].status == "failed" || items[i].status == "done") {
+						idx = $scope.completedjobs.map(function(e) { return e.id; }).indexOf(items[i].id);
+						if(idx >= 0) {
+							$scope.completedjobs[idx] = items[i]
+						} else {
+							$scope.completedjobs.push(items[i]);
+						}
+					} else {
+						idx = $scope.currentjobs.map(function(e) { return e.id; }).indexOf(items[i].id);
+						if(idx >= 0) {
+							$scope.currentjobs[idx] = items[i]
+						} else {
+							$scope.currentjobs.push(items[i]);
+						}
+					}
 				}
 
 			},
@@ -41,32 +59,9 @@ cracklord.controller('JobsController', ['$scope', 'JobsService', 'QueueService',
 		);
 	}
 
-	$scope.reloadJobs = function() {
-		JobsService.query(
-			function(data) {
-				for(var i = 0; i < $scope.jobs.length; i++) {
-					var jobid = $scope.jobs[i].id
-
-					for(var j = 0; j < data.length; j++) {
-						if(data[j].id === $scope.jobs[i].id) {
-							for(var prop in data[j]) {
-								if(data[j].hasOwnProperty(prop)) {
-									$scope.jobs[i][prop] = data[j][prop]
-								}	
-							}
-						}
-					}
-				}
-			}, 
-			function(error) {
-				growl.error("Unable to automatically update job data.")
-			}
-		)
-	}
-
 	//Setup a timer to refresh the data on a regular basis.
 	timer = $interval(function() {
-		$scope.reloadJobs();
+		$scope.loadJobs();
 	}, 15000);
 
 	$scope.$on('$destroy', function() {
@@ -84,18 +79,26 @@ cracklord.directive('jobReorderConfirm', ['QueueService', 'growl', function jobR
 		replace: true,
 		scope: {
 			reload: "&",
-			jobs: "=",
+			current: "=",
+			complete: "=",
 			dragstatus: "="
 		},
 		controller: function($scope) {
 			$scope.reorderConfirm = function() {
-				var ids = $scope.jobs.map(function (job) {
+				var cur_ids = $scope.current.map(function (job) {
+					if(job) {
+						return job.id;
+					}
+				});
+				var done_ids = $scope.complete.map(function (job) {
 					if(job) {
 						return job.id;
 					}
 				});
 
-				QueueService.reorder(ids)
+				total = cur_ids.concat(done_ids)
+
+				QueueService.reorder(total)
 					.success(function(data, status, headers, config) {
 						growl.success("Job data reordered successfully.");
 						$scope.dragstatus = false;
@@ -244,19 +247,6 @@ cracklord.directive('jobDetail', ['JobsService', 'ToolsService', 'growl', 'Resou
 			});
 		},
 	}
-}]);
-
-cracklord.filter('currentJobs', ['JOB_STATUS_RUNNING', 'JOB_STATUS_COMPLETED', function(JOB_STATUS_RUNNING, JOB_STATUS_COMPLETED) {
-	return function(items) {
-		var filtered = [];
-		for (var i = 0; i < items.length; i++) {
-			var item = angularjs.copy(items[i]);
-			if(JOB_STATUS_RUNNING.indexOf(item.status)) {
-				filtered.push(item);
-			}
-		}
-		return filtered;
-	};
 }]);
 
 cracklord.controller('CreateJobController', ['$scope', '$state', 'ToolsService', 'JobsService', 'growl', function CreateJobController($scope, $state, ToolsService, JobsService, growl) {

web/components/Jobs/jobsView.html
@@ -3,7 +3,7 @@
 		<div class="col-md-5 pull-right button-tray"> 
 			<div class="btn-group pull-right" role="group">
 				<a class="btn btn-primary" ng-if="user.checkrole([user.allroles.standard, user.allroles.admin])" ui-sref="createjob.tools"><i class="fa fa-plus"></i> Add Job</a>
-				<job-reorder-confirm reload="loadJobs()" jobs="jobs" dragstatus="listreordered" ng-if="listreordered"></job-reorder-confirm>
+				<job-reorder-confirm reload="loadJobs()" current="currentjobs" complete="completedjobs" dragstatus="listreordered" ng-if="listreordered"></job-reorder-confirm>
 			</div>
 		</div>
 		<h1 class="page-header"><i class="fa fa-stack-overflow"></i>Job Management</h1>
@@ -21,8 +21,8 @@
 					</div>
 				</div>
 			</div>
-			<div class="table-body" id="#joblistcontainer" ui-sortable="sortableOptions" ng-model="jobs">
-				<div ng-repeat="job in jobs | filter: {status: '!quit'} | filter: {status: '!failed'} | filter: {status: '!done'}" ng-class="$index % 2 ? 'odd' : 'even'" class="listrow">
+			<div class="table-body" id="#joblistcontainer" ui-sortable="sortableOptions" ng-model="currentjobs">
+				<div ng-repeat="job in currentjobs track by job.id" class="listrow">
 					<div class="row" style="{{job.resourcecolor}}">
 						<div class="collapser">
 							<a ng-if="job.expanded" ng-click="job.expanded = false" aria-label="detail"><i class="fa fa-minus fa-1x"></i></a>
@@ -68,7 +68,7 @@
 				</div>
 			</div>
 			<div class="table-body" id="#joblistcontainer" ui-sortable="sortableOptions" ng-model="jobs">
-				<div ng-repeat="job in jobs | filter: {status: '!running'} | filter: {status: '!paused'} | filter: {status: '!created'}" ng-class="$index % 2 ? 'odd' : 'even'" class="listrow">
+				<div ng-repeat="job in completedjobs track by job.id" ng-class="$index % 2 ? 'odd' : 'even'" class="listrow">
 					<div class="row">
 						<div class="collapser">
 							<a ng-if="job.expanded" ng-click="job.expanded = false" aria-label="detail"><i class="fa fa-minus fa-1x"></i></a>

commit 0befe1be393024f099be472bc4bc1d3e2db94c06

Solves regression in job listing after b1cf6b7bbfef60973ef72c4889830afc1ba9bca8
After splitting jobs into completed and current arrays, completed jobs
in current weren't being handled.

web/components/Jobs/jobsController.js
@@ -1,291 +1,0 @@
-cracklord.controller('JobsController', ['$scope', 'JobsService', 'QueueService', 'growl', 'ResourceList', '$interval', function JobsController($scope, JobsService, QueueService, growl, ResourceList, $interval) {
-	var timer
-	$scope.listreordered = false;
-	$scope.currentjobs = [];
-	$scope.completedjobs = [];
-	ResourceList.load();
-
-	$scope.sortableOptions = {
-		handle: '.draghandle',
-		axis: 'y',
-		stop: function(e, ui) {
-			$scope.listreordered = true;
-		}
-	};
-
-	$scope.loadJobs = function() {
-		JobsService.query(
-			//Our success handler
-			function(data) {
-				$scope.listreordered = false;
-
-				for(var i = 0; i < data.length; i++) {
-					if(data[i].resourceid) {
-						var id = data[i].resourceid;
-						var resource = ResourceList.get(id);
-						if(resource) {
-							data[i].resourcecolor = "background-color: rgb("+resource.color.r+","+resource.color.g+","+resource.color.b+");";
-						}
-					}
-					data[i].expanded = false;
-					if(items[i].status == "quit" || items[i].status == "failed" || items[i].status == "done") {
-						idx = $scope.completedjobs.map(function(e) { return e.id; }).indexOf(items[i].id);
-						if(idx >= 0) {
-							$scope.completedjobs[idx] = items[i]
-						} else {
-							$scope.completedjobs.push(items[i]);
-						}
-					} else {
-						idx = $scope.currentjobs.map(function(e) { return e.id; }).indexOf(items[i].id);
-						if(idx >= 0) {
-							$scope.currentjobs[idx] = items[i]
-						} else {
-							$scope.currentjobs.push(items[i]);
-						}
-					}
-				}
-
-			},
-			//Our error handler
-			function(error) {
-				switch (error.status) {
-					case 400: growl.error("You sent bad data, check your input and if it's correct get in touch with us on github"); break;
-					case 403: growl.warning("You're not allowed to do that..."); break;
-					case 404: growl.error("That object was not found."); break;
-					case 409: growl.error("The request could not be completed because there was a conflict with the existing resource."); break;
-					case 500: growl.error("An internal server error occured while trying to add the resource."); break;
-				}
-			}
-		);
-	}
-
-	//Setup a timer to refresh the data on a regular basis.
-	timer = $interval(function() {
-		$scope.loadJobs();
-	}, 15000);
-
-	$scope.$on('$destroy', function() {
-		$interval.cancel(timer)
-	});
-
-	//Initially we'll also load our data	
-	$scope.loadJobs();
-}]);
-
-cracklord.directive('jobReorderConfirm', ['QueueService', 'growl', function jobReorderConfirm(QueueService, growl) {
-	return {
-		restrict: 'E',
-		templateUrl: 'components/Jobs/jobsReorderConfirm.html', 
-		replace: true,
-		scope: {
-			reload: "&",
-			current: "=",
-			complete: "=",
-			dragstatus: "="
-		},
-		controller: function($scope) {
-			$scope.reorderConfirm = function() {
-				var cur_ids = $scope.current.map(function (job) {
-					if(job) {
-						return job.id;
-					}
-				});
-				var done_ids = $scope.complete.map(function (job) {
-					if(job) {
-						return job.id;
-					}
-				});
-
-				total = cur_ids.concat(done_ids)
-
-				QueueService.reorder(total)
-					.success(function(data, status, headers, config) {
-						growl.success("Job data reordered successfully.");
-						$scope.dragstatus = false;
-						$scope.reload();
-					})
-					.error(function(data, status, headers, config) {
-						switch (status) {
-							case 400: growl.error("You sent bad data, check your input and if it's correct get in touch with us on github"); break;
-							case 403: growl.warning("You're not allowed to do that..."); break;
-							case 404: growl.error("Somehow the queue object was not found... this is bad."); break;
-							case 409: growl.error("The request could not be completed because there was a conflict."); break;
-							case 500: growl.error("An internal server error occured while trying to reorder the queue."); break;
-						}
-						$scope.dragstatus = false;
-					});
-			}
-
-			$scope.reorderCancel = function() {
-				$scope.dragstatus = false;
-				$scope.reload();
-				growl.info("Reordering of jobs cancelled.")
-			}
-		}
-	}
-}]);
-
-cracklord.directive('jobDetail', ['JobsService', 'ToolsService', 'growl', 'ResourceList', '$interval', '$q', function jobDetail(JobsService, ToolsService, growl, ResourceList, $interval, $q) {
-	return {
-		restrict: 'E',
-		templateUrl: 'components/Jobs/jobsViewDetail.html',
-		scope: {
-			jobid: '@',
-			visibility: '='
-		},
-		controller: function($scope) {
-			// Mmmmmmmm.... Donut.
-			$scope.processDonut = function(animate) {
-				$scope.donut = {};
-				$scope.donut.labels = ['Processed', 'Remaining'];
-
-				var total = 100 - $scope.detail.progress;
-				$scope.donut.data = [$scope.detail.progress, total];
-				$scope.donut.colors = [ '#337ab7', '#aaaaaa' ];
-				$scope.donut.options = {
-					'animateRotate': animate,
-					'animation': animate
-				}
-			};
-
-			$scope.processLine = function(animate) {
-				$scope.line = {};
-				$scope.line.series = [ $scope.detail.performancetitle ]; 
-				$scope.line.data = [];
-				$scope.line.data[0] = [];
-				$scope.line.labels = [];
-				$scope.line.options = {
-					'pointDot': false,
-					'showTooltips': false,
-					'animation': animate
-				};
-				$scope.line.colors = [
-					'#d43f3a'
-				]
-
-				var sorted_times = Object.keys($scope.detail.performancedata).sort()
-				var len = sorted_times.length - 1
-				var min = Math.max(0, len-240)
-				var step = len > 60 ? 3 : 1
-				for (var i = min; i <= len; i=i+step) {
-					time = sorted_times[i]
-					$scope.line.data[0].push($scope.detail.performancedata[time]);
-					if((i - min) % 20 == 0) {
-						var date = new Date(time * 1000)
-						var m = ('0'+date.getMinutes()).slice(-2);
-						var h = date.getHours();
-						var ampm = h > 12 ? "PM" : "AM";
-						h = h > 12 ? h - 12 : h;
-						$scope.line.labels.push(h + ":" + m + " " + ampm);
-					} else {
-						$scope.line.labels.push("");
-					}
-				};
-			}
-		},
-		link: function($scope, $element, $attrs) {
-			var timer
-
-			$scope.loadData = function(animate) {
-				return $q(function(resolve, reject) {
-					JobsService.get({id: $scope.jobid}, 
-						function success(data) {
-							$scope.detail = data.job;
-							$scope.processDonut(animate);
-							$scope.processLine(animate);
-							resolve();
-						},
-						function error(error) {
-							growl.error("There was a problem loading job details.")
-							$($element).find('div.slider').slideUp("slow", function() {
-								$element.parent().hide();
-							});
-							reject();
-						}
-					);	
-				})
-			}
-
-			$scope.$watch('visibility', function(newval, oldval) {
-				if(newval === true) {
-					$scope.loadData(true).then(function() {
-						if($scope.detail) {
-							var resource = ResourceList.get($scope.detail.resourceid);
-							if(resource) {
-								$scope.resource = {}
-								$scope.resource.name = resource.name;
-							}
-
-							ToolsService.get({id: $scope.detail.toolid}, 
-								function toolsuccess(data) {
-									$scope.tool = data.tool;
-								}
-							);
-						}
-
-						$element.parent().show();
-						$element.find('.slider').slideDown();
-
-						if(!timer) {
-							timer = $interval(function() {
-								//Disable the reload animation at this point
-								$scope.loadData(false);
-							}, 15000);
-						}
-					})
-				} else {
-					$interval.cancel(timer)
-					timer = null
-					$($element).find('div.slider').slideUp("slow", function() {
-						$element.parent().hide();
-					});
-				}
-			});
-
-			$scope.$on('$destroy', function() {
-				$interval.cancel(timer)
-			});
-		},
-	}
-}]);
-
-cracklord.controller('CreateJobController', ['$scope', '$state', 'ToolsService', 'JobsService', 'growl', function CreateJobController($scope, $state, ToolsService, JobsService, growl) {
-	$scope.formData = {};
-	$scope.formData.params = {};
-
-	$scope.toolChange = function() {
-		var id = $scope.formData.tool.id;
-		var tool = ToolsService.get({id: id}, 
-			function(data) {
-				$scope.tool = data.tool;
-			}, 
-			function(error) {
-				growl.error("An error occured while trying to load tool information.");
-			}
-		);
-	}
-
-	$scope.processNewJobForm = function() {
-		var newjob = new JobsService();
-
-		newjob.toolid = $scope.formData.tool.id;
-		newjob.name = $scope.formData.name;
-		newjob.params = $scope.formData.params;
-		
-		JobsService.save(newjob, 
-			function(data) {
-				growl.success("Job successfully added");
-				$state.transitionTo('jobs');
-			}, 
-			function(error) {
-				switch (error.status) {
-					case 400: growl.error("You sent bad data, check your input and if it's correct get in touch with us on github"); break;
-					case 403: growl.warning("You're not allowed to do that..."); break;
-					case 404: growl.error("That object was not found."); break;
-					case 409: growl.error("The request could not be completed because there was a conflict with the existing resource."); break;
-					case 500: growl.error("An internal server error occured while trying to add the resource."); break;
-				}
-			}
-		);
-	}	
-}]);