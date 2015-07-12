ca164e74ea48e3b47243046ccaf4cd8ab41699e9
emperorcow <emperorcow@gmail.com>
2015-07-11 20:21:34 -0500

    Finished direct connect resource manager plugin

--- plugins/resourcemanagers/directconnect/directconnect.go
+++ plugins/resourcemanagers/directconnect/directconnect.go
@@ -6,22 +6,25 @@ import (
 	log "github.com/Sirupsen/logrus"
 	"github.com/emperorcow/protectedmap"
 	"github.com/jmmcatee/cracklord/common/queue"
+	"strconv"
 )
 
 type resourceInfo struct {
-	Notes         string
-	ReconnectTime int
+	Notes     string
+	Reconnect bool
 }
 
 type directResourceManager struct {
 	resources protectedmap.ProtectedMap
 	q         *queue.Queue
+	tls       *tls.Config
 }
 
-func Setup(qpointer *queue.Queue) queue.ResourceManager {
+func Setup(qpointer *queue.Queue, tlspointer *tls.Config) queue.ResourceManager {
 	return &directResourceManager{
 		resources: protectedmap.New(),
 		q:         qpointer,
+		tls:       tlspointer,
 	}
 }
 
@@ -39,32 +42,7 @@ func (this directResourceManager) Description() string {
 
 func (this directResourceManager) Parameters() string {
 	return `"form": [
-	    {
-	        "type": "section",
-	        "htmlClass": "row",
-	        "items": [
-	            {
-	                "type": "section",
-	                "htmlClass": "col-xs-6",
-	                "items": [
-	                    "reconnect"
-	                ]
-	            },
-	            {
-	                "type": "section",
-	                "htmlClass": "col-xs-6",
-	                "items": [
-	                    {
-	                        "type": "conditional",
-	                        "condition": "modelData.reconnect",
-	                        "items": [
-	                            "reconnecttime"
-	                        ]
-	                    }
-	                ]
-	            }
-	        ]
-	    },
+        "reconnect",
 	    {
 	        "key": "notes",
 	        "type": "textarea",
@@ -83,20 +61,14 @@ func (this directResourceManager) Parameters() string {
 	            "title": "Attempt automatic reconnect?",
 	            "type": "boolean",
 	            "default": true
-	        },
-	        "reconnecttime": {
-	            "title": "Reconnect Time",
-	            "description": "In seconds",
-	            "type": "integer",
-	            "default": 10
 	        }
 		}
 	}`
 }
 
-func (this *directResourceManager) AddResource(name string, address string, params map[string]string, tls *tls.Config) error {
+func (this *directResourceManager) AddResource(name string, address string, params map[string]string) error {
 	//First, we attempt to add the resource into the queue itself
-	uuid, err := this.q.AddResource(address, name, tls)
+	uuid, err := this.q.AddResource(address, name, this.tls)
 
 	//If unable to connect, log it and return the error to the API
 	if err != nil {
@@ -104,19 +76,8 @@ func (this *directResourceManager) AddResource(name string, address string, para
 		return err
 	}
 
-	//Let's create a temporary resource to hold the info
-	tempresource = resourceInfo{
-		ReconnectTime: -1,
-		Notes:         params[notes],
-	}
-
-	//If we were going to try and reconnect (from the boolean parameter), then set the time to the value.
-	if params[reconnect] == true {
-		tempresource.ReconnectTime = params[reconnecttime]
-	}
-
 	//Finally, set the resource into our map
-	this.resources.Set(uuid, tempresource)
+	this.resources.Set(uuid, this.parseParams(params))
 
 	return nil
 }
@@ -136,22 +97,112 @@ func (this *directResourceManager) DeleteResource(resourceid string) error {
 	return nil
 }
 
-func (this directResourceManager) GetResource(resourceid string) (queue.Resource, map[string]string, error) {
-	return queue.Resource{}, nil
+func (this directResourceManager) GetResource(resourceid string) (*queue.Resource, map[string]string, error) {
+	//First, get the resource itself from the queue
+	resource, ok := this.q.GetResource(resourceid)
+
+	//If we weren't able to gather it, return an error
+	if !ok {
+		return &queue.Resource{}, nil, errors.New("Resource with requested ID not found in the queue.")
+	}
+
+	//Now we'll gather the data from our local map of parameters
+	localresource, ok := this.resources.Get(resourceid)
+	if !ok {
+		return &queue.Resource{}, nil, errors.New("Resource wiht requested ID could not be found in direct connect resource manager.")
+	}
+
+	//Since our map uses a generic interface{}, we have to cast our result back
+	localres := localresource.(resourceInfo)
+
+	//Parse our parameters struct back into a common string map
+	parameters := make(map[string]string)
+	parameters["notes"] = localres.Notes
+	parameters["reconnect"] = strconv.FormatBool(localres.Reconnect)
+
+	return resource, parameters, nil
 }
 
-func (this *directResourceManager) PauseResource(resourceid string) error {
-	return nil
-}
+func (this *directResourceManager) UpdateResource(resourceid string, newstatus string, newparams map[string]string) error {
+	//Because we need to make some comparisons for pause/resume, let's get the current resource state
+	oldresource, _, err := this.GetResource(resourceid)
+	if err != nil {
+		return err
+	}
 
-func (this *directResourceManager) ResumeResource(resourceid string) error {
+	//Set the internal parameters within the direct connect manager to the new data
+	this.resources.Set(resourceid, this.parseParams(newparams))
+
+	//Check to see if the old status matches the new one, if not, we need to make a change
+	if oldresource.Status != newstatus {
+		switch newstatus {
+		case "resume": //If our new status is resume, then resume the resource
+			err = this.q.ResumeResource(resourceid)
+			if err != nil {
+				return err
+			}
+			break
+
+		case "pause": //If the new status is pause, pause the resource in the queue
+			err = this.q.PauseResource(resourceid)
+			if err != nil {
+				return err
+			}
+			break
+		}
+	}
+
+	//Finally, we can return a nil as we were successful
 	return nil
 }
 
 func (this directResourceManager) GetManagedResources() []string {
-	return []string{"one", "two"}
+	//We need to make a slice of resource UUID strings for every resource we manage.  First, let's make the actual slice with a length of the size of our map
+	resourceids := make([]string, this.resources.Count())
+
+	//Next let's start up an iterator for our map and loop through each resource
+	iter := this.resources.Iterator()
+	for data := range iter.Loop() {
+		//Now let's add the ID from the map to the slice of UUIDs
+		resourceids = append(resourceids, data.Key)
+	}
+
+	return resourceids
 }
 
+//This function loops through all of the directly connected resources and detects
+//that resource is still connected.  If so, it will do nothing; however, if not
+//then it will attempt to reconnect if at all possible.
 func (this *directResourceManager) Keep() {
-	return
+	iter := this.resources.Iterator()
+	for data := range iter.Loop() {
+		localResource := data.Val.(resourceInfo)
+		queueResource, ok := this.q.GetResource(data.Key)
+		if !ok {
+			log.WithField("resourceid", data.Key).Error("Unable to find a resource in the queue that the direct connect manager thought it was responsible for.")
+			continue
+		}
+		status := this.q.CheckResourceConnectionStatus(queueResource)
+		if !status && localResource.Reconnect {
+			log.WithField("resource", queueResource.Name).Info("Attempting to reconnect to directly connected resource.")
+			this.q.ConnectResource(queueResource, this.tls)
+		}
+	}
+
+	log.Info("Direct connect resource manager has successfully updated resources.")
+}
+
+func (this *directResourceManager) parseParams(params map[string]string) resourceInfo {
+	//Let's create a temporary resource to hold the info
+	tempresource := resourceInfo{
+		Reconnect: false,
+		Notes:     params["notes"],
+	}
+
+	//Set the boolean parameter for reconnect if true.
+	if params["reconnect"] == "true" {
+		tempresource.Reconnect = true
+	}
+
+	return tempresource
 }
