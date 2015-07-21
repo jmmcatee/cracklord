56eef2d39abbd2adfbc6e104bbd171947533df49
Lucas Morris <emperorcow@gmail.com>
2015-07-21 08:56:16 -0500

    Updates to plugins on index

---
+++ index.js
@@ -0,0 +1,16 @@
+var index = angular.module('index', []);
+index.controller('PluginController', ['$scope', '$http', function ($scope, $http) {
+	$scope.plugins = {}
+	$http.get('plugins/tools.json').success(function(data) {
+		$scope.plugins.tools = data;
+	});
+	$http.get('plugins/resourcemanagers.json').success(function(data) {
+		$scope.plugins.resourcemanagers = data;
+	});
+}]);
+
+$('body').scrollspy({ target: '#navbar' })
+$('.carousel').carousel({
+	interval: 5000,
+	pause: "false"
+})
