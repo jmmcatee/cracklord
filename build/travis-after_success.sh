97a779dfd860e92b7f94c25be5c24336119aa314
Lucas Morris <emperorcow@gmail.com>
2017-03-26 13:14:09 -0500

    4

--- build/travis-after_success.sh
+++ build/travis-after_success.sh
@@ -13,7 +13,7 @@ WEBDIR="./web"
 URL="http://jmmcatee.github.io/cracklord/"
 MAINT="emperorcow@gmail.com"
 
-echo "Creating queue server package directories"
+echo -n "Creating queue server package directories..."
 mkdir -p $QDST/usr/bin 
 mkdir -p $QDST/etc/cracklord
 cp -r $QSRC/conf/* $QDST/etc/cracklord/
@@ -21,15 +21,22 @@ mkdir -p $QDST/var/cracklord/www
 cp -r $BASEDIR/web $QDST/var/cracklord/www
 mkdir -p $QDST/etc/init
 cp -r $QSRC/cracklord-queued.conf $QDST/etc/init/
+echo "done"
 
-echo "Creating resource server package directories"
+echo -n "Creating resource server package directories"
 mkdir -p $RDST/usr/bin
 mkdir -p $RDST/etc/cracklord
 cp -r $RSRC/conf/* $RDST/etc/cracklord/
 mkdir -p $RDST/etc/init
 cp -r $RSRC/cracklord-resourced.conf $RDST/etc/init/
+echo "done"
 
-echo -n "Generating packages using FPM"
-fpm -s dir -t deb -n "cracklord-queued" -v "$VER" --before-install $QSRC/beforeinstall.sh --after-install $QSRC/afterinstall.sh --before-remove $QSRC/beforeremove.sh --after-remove $QSRC/afterremove.sh --url "$URL" --config-files etc/cracklord --description "CrackLord job management queue server" -m "$MAINT" -C $QSRC usr etc var
+echo -n "Generating queue package using FPM"
+fpm -s dir -t deb -n "cracklord-queued" -v "$VER" --before-install $QSRC/beforeinstall.sh --after-install $QSRC/afterinstall.sh --before-remove $QSRC/beforeremove.sh --after-remove $QSRC/afterremove.sh --url "$URL" --config-files etc/cracklord --description "CrackLord job management queue server" -m "$MAINT" -C $QDST usr etc var
+echo "done"
+
+echo -n "Generating resource package using FPM"
 fpm -s dir -t deb -n "cracklord-resourced" -v "$VER" --before-install $RESOURCESRC/beforeinstall.sh --after-install $RESOURCESRC/afterinstall.sh --before-remove $RESOURCESRC/beforeremove.sh --after-remove $RESOURCESRC/afterremove.sh --url "$URL" --config-files etc/cracklord --description "Cracklord job management system resource server" -m "$MAINT" -C $RDST usr etc
+echo "done"
 
+ls
