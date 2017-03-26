a7f38468421418629127a35590854ef630991621
Lucas Morris <emperorcow@gmail.com>
2017-03-26 13:09:25 -0500

    test 3

--- build/travis-after_success.sh
+++ build/travis-after_success.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 VER=`git describe --dirty --always --tags`
-BASEDIR="github.com/jmmcatee/cracklord"
+BASEDIR="."
 QSRC="$BASEDIR/build/queued/"
 RSRC="$BASEDIR/build/resourced/"
 
@@ -16,9 +16,9 @@ MAINT="emperorcow@gmail.com"
 echo "Creating queue server package directories"
 mkdir -p $QDST/usr/bin 
 mkdir -p $QDST/etc/cracklord
-cp -r ub.com/conf/* $QDST/etc/cracklord/
+cp -r $QSRC/conf/* $QDST/etc/cracklord/
 mkdir -p $QDST/var/cracklord/www
-cp -r web $QDST/var/cracklord/www
+cp -r $BASEDIR/web $QDST/var/cracklord/www
 mkdir -p $QDST/etc/init
 cp -r $QSRC/cracklord-queued.conf $QDST/etc/init/
 
@@ -30,7 +30,6 @@ mkdir -p $RDST/etc/init
 cp -r $RSRC/cracklord-resourced.conf $RDST/etc/init/
 
 echo -n "Generating packages using FPM"
-FPM=`rbenv which fpm`
-$FPM -s dir -t deb -n "cracklord-queued" -v "$VER" --before-install $QSRC/beforeinstall.sh --after-install $QSRC/afterinstall.sh --before-remove $QSRC/beforeremove.sh --after-remove $QSRC/afterremove.sh --url "$URL" --config-files etc/cracklord --description "CrackLord job management queue server" -m "$MAINT" -C $QSRC usr etc var
-$FPM -s dir -t deb -n "cracklord-resourced" -v "$VER" --before-install $RESOURCESRC/beforeinstall.sh --after-install $RESOURCESRC/afterinstall.sh --before-remove $RESOURCESRC/beforeremove.sh --after-remove $RESOURCESRC/afterremove.sh --url "$URL" --config-files etc/cracklord --description "Cracklord job management system resource server" -m "$MAINT" -C $RDST usr etc
+fpm -s dir -t deb -n "cracklord-queued" -v "$VER" --before-install $QSRC/beforeinstall.sh --after-install $QSRC/afterinstall.sh --before-remove $QSRC/beforeremove.sh --after-remove $QSRC/afterremove.sh --url "$URL" --config-files etc/cracklord --description "CrackLord job management queue server" -m "$MAINT" -C $QSRC usr etc var
+fpm -s dir -t deb -n "cracklord-resourced" -v "$VER" --before-install $RESOURCESRC/beforeinstall.sh --after-install $RESOURCESRC/afterinstall.sh --before-remove $RESOURCESRC/beforeremove.sh --after-remove $RESOURCESRC/afterremove.sh --url "$URL" --config-files etc/cracklord --description "Cracklord job management system resource server" -m "$MAINT" -C $RDST usr etc
 
