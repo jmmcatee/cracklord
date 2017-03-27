5d28b3cf349d8b04a8f98dd3dc7e711786c3c04c
Lucas Morris <emperorcow@gmail.com>
2017-03-27 07:01:57 -0500

    7

--- build/travis-deploy.sh
+++ build/travis-deploy.sh
@@ -5,6 +5,10 @@ distros = (
   "debian/jessie"
 )
 
+ls
+
 for i in "${distros[@]}"; do
+	echo -n "Pushing files to packagecloud for $i..."
 	package_cloud push emperorcow/cracklord/$i *.deb
+	echo "done"
 done
